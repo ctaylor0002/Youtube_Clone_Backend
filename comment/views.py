1d88e050ed0bd4e586c07acd0f13681d7d828566
ctaylor0002 <charles4312@comcast.net>
2022-11-18 16:03:18 -0500

    Created GET Request For Video_Id To Get All Comments Associated

--- comment/views.py
+++ comment/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from .models import Comment
+from .serializers import CommentSerializer
+
+# New Imports
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.decorators import api_view, permission_classes
 
 # Create your views here.
+
+@api_view(['GET'])
+@permission_classes([AllowAny])
+def get_comments_for_video(request):
+    type_param = request.query_params.get('video_id')
+    
+    comments = Comment.objects.all()
+
+    if type_param:
+        comments = comments.filter(video_id__video_id=type_param)
+
+    serializer = CommentSerializer(comments, many=True)
+    return Response(serializer.data, status=status.HTTP_200_OK)
