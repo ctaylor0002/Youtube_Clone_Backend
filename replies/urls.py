ad2bb910f7cd32ec282789d6606d0e9aba72ce99
ctaylor0002 <charles4312@comcast.net>
2022-11-19 15:23:15 -0500

    Created A GET Method to Get All Replies Based On Comment Id

--- replies/urls.py
+++ replies/urls.py
@@ -2,6 +2,6 @@ from django.urls import path, include
 from replies import views
 
 urlpatterns = [
-    path('<int:pk>/', views.post_reply),
+    path('<int:pk>/', views.reply_list),
    
 ]
--- replies/views.py
+++ replies/views.py
@@ -8,13 +8,11 @@ from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.decorators import api_view, permission_classes
 # Create your views here.
 
-@api_view(['POST'])
+@api_view(['POST', 'GET'])
 @permission_classes([IsAuthenticated])
-def post_reply(request, pk):
-    if request.method == 'POST':
-        #updated_request = {'text':(request.data['text']), 'comment_id': pk}
+def reply_list(request, pk):
 
-       
+    if request.method == 'POST':
 
         serializer = ReplySerializer(data=request.data)
 
@@ -22,3 +20,9 @@ def post_reply(request, pk):
             serializer.save(user=request.user, comment_id = pk)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif(request.method == 'GET'):
+        replies = Reply.objects.filter(comment_id=pk)
+        serializer = ReplySerializer(replies, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
