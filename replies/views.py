44e229850a1aecc15de950affe7444c9f0d0638c
ctaylor0002 <charles4312@comcast.net>
2022-11-19 14:50:01 -0500

    Created Reply Model

---
+++ replies/views.py
@@ -0,0 +1,7 @@
+from rest_framework import status
+from rest_framework.response import Response
+from .models import Reply
+from .serializers import ReplySerializer
+from django.shortcuts import get_object_or_404
+
+# Create your views here.
